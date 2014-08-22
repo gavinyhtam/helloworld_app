4fc30622c0092f9375a0a1c670ff3edee5f4e416
Gavin Tam <gavintam168@gmail.com>
2014-08-22 22:13:29 +1000

    Added support for FB date to timeline

---
+++ app/assets/javascripts/fb-photos.js
@@ -0,0 +1,32 @@
+//Sourced from http://www.jqueryscript.net/social-media/jQuery-Plugin-To-Display-Photos-From-Your-Facebook-Album-Facebook-Photos.html
+(function ($) {
+    $.fn.facebookAlbumPhotos = function (options) {
+        var settings = $.extend(options);
+        return this.each(function () {
+            var albumID = settings.albumURL.replace(/.*(a\.)([0-9]+).*/, "$2");
+            var photoLimit = settings.limit || 10;
+            var target = $(this);
+            var imgStyle = settings.CSS || '';
+            var imgSizes = {
+                'original': 1,
+                'huge': 2,
+                'x-large': 3,
+                'large': 4,
+                'medium': 5,
+                'small': 6,
+                'x-small': 7
+            };
+            var imgSize = imgSizes[settings.imgSize] || 5;
+            $.getJSON('https://graph.facebook.com/' + albumID + '/photos?callback=?', function (json, status, xhr) {
+                if (json.data.length < photoLimit) {
+                    photoLimit = json.data.length;
+                }
+                console.log(json.data);
+                target.html('');
+                for (i = json.data.length - 1; i >= json.data.length - photoLimit; i--) {
+                    target.append("<img src='" + json.data[i].images[imgSize].source + "' alt='Slideshow image' style='" + imgStyle + "'/>");
+                }
+            });
+        });
+    };
+})(jQuery);
