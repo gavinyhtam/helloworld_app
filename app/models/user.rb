250907d4928e8b66b345fea4b17c7aead9500f47
Gavin Tam <gavintam168@gmail.com>
2014-08-31 18:37:41 +1000

    Added simple database for User

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ActiveRecord::Base
+	before_save { username.downcase! } #ensure username is stored as lowercase
+
+	VALID_USERNAME_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
+	validates :username, 
+		presence: true, 
+		uniqueness: {case_sensitive: false},
+		format: {with: VALID_USERNAME_REGEX}
+end
