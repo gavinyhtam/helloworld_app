b5ec2eed69b4bcb3727d3cc81c9a3d638da25eb8
Gavin Tam <gavintam168@gmail.com>
2014-09-09 14:02:37 +1000

    Minor bug fixes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,5 @@
 class StaticPagesController < ApplicationController
-  protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json' }
+ skip_before_filter :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' }
   def home
   end
 
--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,18 +1,19 @@
 class TodosController < ApplicationController
-	skip_before_filter :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' }
+ skip_before_filter :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' }
 	include LocationCode
   def index
-		@user = User.find_or_create_by(username: session[:username])	#find or create the logged in user
+		@user = User.find_or_create_by(username: cookies[:username])	#find or create the logged in user
 		@locations = []
 		@user.photos.each do |photo|
 			#add unique locations to the @locations (inefficient but will change to use a set instead)
 			@locations.push(code(photo.location_name)) unless @locations.include? code(photo.location_name)
 		end
-		#@locations.push("KR","SG")	#hardcode countries to colour on the map first
+		@locations.push("KR","SG")	#hardcode countries to colour on the map first
   end
 
 	def friends
-		@user = User.find_or_create_by(username: session[:username])	#find or create the logged in user
+		@user = User.find_or_create_by(username: cookies[:username])	#find or create the logged in user
+		stylesheets << '3-col-portfolio'
 	end
 	
 	def create
@@ -22,7 +23,7 @@ class TodosController < ApplicationController
 	end
 
 	def createPhoto
-		@user = User.find_by(username: session[:username])
+		@user = User.find_by(username: cookies[:username])
 		#create the photo from the given attributes
 		@user.photos.build(link: params[:link], location_name: params[:location_name], date: params[:date], photo_name: params[:photo_name])
 		if @user.save	#if the photo was saved successfully
