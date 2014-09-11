bf748907dd89c37a3e406e28729b16fa42ffb76c
Gavin Tam <gavintam168@gmail.com>
2014-09-11 16:53:52 +1000

    Added gallery to profile page

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,6 +3,8 @@ class ProfilesController < ApplicationController
 		@user = User.find_by(username: cookies[:username])
 		if @user.photos.size > 1
 			@last_location = @user.photos.order(:date).find(1);
+			@last_location_photos = @user.photos.where(location_name: @last_location.location_name);
+			puts @last_location_photos
 			@locations = []
 			@user.photos.each do |photo|
 				#add unique locations to the @locations (inefficient but will change to use a set instead)
