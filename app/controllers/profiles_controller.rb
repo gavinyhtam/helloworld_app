8e166a90d59b5f7f16b2350d92c7953f30355742
Gavin Tam <gavintam168@gmail.com>
2014-09-11 13:05:39 +1000

    Formatted TravelDex page

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,6 +3,11 @@ class ProfilesController < ApplicationController
 		@user = User.find_by(username: cookies[:username])
 		if @user.photos.size > 1
 			@last_location = @user.photos.order(:date).find(1);
+			@locations = []
+			@user.photos.each do |photo|
+				#add unique locations to the @locations (inefficient but will change to use a set instead)
+				@locations.push(photo.location_name) unless @locations.include? photo.location_name
+			end
 		end
   end
 end
